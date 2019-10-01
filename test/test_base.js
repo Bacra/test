ecb43bd89e77b8d2772a54a65d764df0728db1e1
bacrawu <bacrawu@tencent.com>
2019-10-01 21:43:22 +0800

    init

--- test/test_base.js
+++ test/test_base.js
@@ -1,3 +1,8 @@
+const expect = require('expect.js');
+const Test = require('../');
+
 describe('#base', function() {
-    it('#base', function() {});
+    it('#base', function() {
+        expect(Test()).to.be(123);
+    });
 });
